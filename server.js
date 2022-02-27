4627693c3d6e5ad716c44d4343b32a59dc12d069
levigun <t.levina31@gmail.com>
2022-02-27 21:17:38 +1100

    added function init to server.js file

--- server.js
+++ server.js
@@ -1,1 +1,36 @@
 const connection = require("./lib/connection");
+const inquirer = require('inquirer');
+const questions = require('./lib/questions');
+
+init();
+
+async function init() {
+    const { action } = await inquirer.prompt(questions);
+    switch (action) {
+        case "Update Department":
+            updateDepartment();
+        case "Update Roles":
+            updateRoles();
+        case "Update Employee":
+            updateEmployee();
+        case "View Information":
+            viewInfo();
+        case "Exit",
+            process.exit(0):
+            break;
+        default:
+            break;
+    }
+}
+
+
+// update department function
+
+// update roles function
+
+// update employee function
+
+// view information function
+
+// exit functi
+
